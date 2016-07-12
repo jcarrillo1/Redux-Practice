c10d5d1e24f075be2ad7d8be975755e9cae788d6
jcarrillo1 <xjcarrillox@gmail.com>
2016-07-12 14:42:25 -0400

    Progress

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -2,6 +2,7 @@ import React, { PropTypes, Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as courseActions from '../../actions/courseActions';
+import CourseList from './CourseList';
 
 class CoursesPage extends Component {
 	constructor(props, context) {
@@ -9,39 +10,18 @@ class CoursesPage extends Component {
 		this.state = {
 			course: { title: '' }
 		};
-		this.onTitleChange = this.onTitleChange.bind(this);
-		this.onClickSave = this.onClickSave.bind(this);
-	}
-
-	onTitleChange(e) {
-		const course = this.state.course;
-		course.title = e.target.value;
-		this.setState({ course });
-	}
-
-	onClickSave() {
-		this.props.actions.createCourse(this.state.course);
 	}
 
 	courseRow(course, index) {
 		return <div key={index}>{course.title}</div>;
 	}
+
 	render() {
+		const { courses } = this.props;
 		return (
 			<div>
-				<h1>Courses</h1>
-				{this.props.courses.map(this.courseRow)}
-				<h2>Add Course</h2>
-				<input
-					type="text"
-					onChange={this.onTitleChange}
-					value={this.state.course.title}
-				/>
-				<input
-					type="submit"
-					value="Save"
-					onClick={this.onClickSave}
-				/>
+				<h1>Course</h1>
+				<CourseList courses={courses} />
 			</div>
 		);
 	}
